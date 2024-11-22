99b96baa7ed91df355e13cfb908c7b0a0c3ee815
Aviran Abramov <aviranabramov@gmail.com>
2024-11-22 14:32:53 +0200

    Add data of supplements in seeders/data

---
+++ database/seeders/data/supplements.php
@@ -0,0 +1,88 @@
+<?php
+
+return [
+    [
+        "name" => "Ashwagandha",
+        "description" => "Herb that promotes relaxation, helps with mental clarity, reduces stress and cortisol levels.",
+    ],
+    [
+        "name" => "Omega 3",
+        "description" => "Supports heart health, improves brain function, reduces inflammation, supports mood, skin, and joint health.",
+    ],
+    [
+        "name" => "Creatine Monohydrate",
+        "description" => "Boosts muscle strength, enhances exercise performance, supports energy production, aids recovery, and improves muscle growth.",
+    ],
+    [
+        "name" => "Vitamin D3K2",
+        "description" => "Supports bone health, boosts immunity, improves calcium absorption, prevents artery calcification, and promotes heart health.",
+    ],
+    [
+        "name" => "Magnesium Glycinate",
+        "description" => "Improves sleep, reduces stress, supports muscle relaxation, boosts heart health, enhances mood, and promotes better digestion.",
+    ],
+    [
+        "name" => "Apple Cider Vinegar",
+        "description" => "Aids digestion, supports weight management, balances blood sugar, boosts skin health, and promotes heart wellness.",
+    ],
+    [
+        "name" => "Collagen Peptides",
+        "description" => "Enhances skin elasticity, strengthen hair, improves joint health, supports bone density, and boosts muscle recovery.",
+    ],
+    [
+        "name" => "Inulin",
+        "description" => "Supports gut health, improves digestion, promotes regularity, boosts beneficial gut bacteria, enhances calcium absorption, and stabilizes blood sugar.",
+    ],
+    [
+        "name" => "NAC",
+        "description" => "Supports liver detoxification, boosts antioxidant levels, improves lung health, enhances immunity, and promotes mental clarity.",
+    ],
+    [
+        "name" => "Extra Virgin Olive Oil",
+        "description" => "Supports heart health, reduces inflammation, boosts brain function, enhances skin health, and promotes longevity.",
+    ],
+    [
+        "name" => "NMN",
+        "description" => "Boosts cellular energy, supports DNA repair, enhances brain health, promotes longevity, and improves metabolic function.",
+    ],
+    [
+        "name" => "Turmeric",
+        "description" => "Reduces inflammation, supports joint health, boosts immunity, enhances digestion, improves skin health, and promotes brain function.",
+    ],
+    [
+        "name" => "Zinc Citrate",
+        "description" => "Boosts immunity, supports wound healing, enhances skin health, improves digestion, promotes hormone balance, and aids enzyme function.",
+    ],
+    [
+        "name" => "Tongkat Ali",
+        "description" => "Enhances energy levels, boosts testosterone, improves mood, supports muscle growth, promotes libido, and reduces stress.",
+    ],
+    [
+        "name" => "Magnesium L-Threonate",
+        "description" => "Improves cognitive function, enhances memory, supports brain health, reduces stress, promotes relaxation, and boosts overall mental clarity.",
+    ],
+    [
+        "name" => "L-Theanine",
+        "description" => "Promotes relaxation, reduces stress, enhances focus, improves sleep quality, boosts mood, and supports cognitive performance.",
+    ],
+    [
+        "name" => "Apigenin",
+        "description" => "Promotes relaxation, supports sleep quality, reduces inflammation, boosts brain health, enhances mood, and supports cardiovascular health.",
+    ],
+    [
+        "name" => "Inositol",
+        "description" => "Supports mental health, improves mood, enhances hormonal balance, promotes metabolic health, aids fertility, and boosts overall brain function.",
+    ],
+    [
+        "name" => "Alpha-GPC",
+        "description" => "Enhances cognitive function, boosts memory, supports focus, promotes brain health, improves learning ability, and aids physical performance.",
+    ],
+    [
+        "name" => "Glycine",
+        "description" => "Supports sleep quality, promotes collagen production, enhances brain health, boosts detoxification, aids muscle recovery, and improves digestion.",
+    ],
+    [
+        "name" => "Taurine",
+        "description" => "Supports heart health, enhances energy production, boosts brain function, promotes hydration, reduces oxidative stress, and improves exercise performance.",
+    ],
+];
