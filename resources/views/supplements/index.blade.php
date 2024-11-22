393bc7255fec90fb390a2b9e1095a57a4b8d37f9
Aviran Abramov <aviranabramov@gmail.com>
2024-11-22 17:18:24 +0200

    Create cards for each supplement in index

--- resources/views/supplements/index.blade.php
+++ resources/views/supplements/index.blade.php
@@ -1,3 +1,12 @@
 <x-layout metaTitle="Index" title="Supplements">
-    hi
+    <section>
+        <div class="flex flex-wrap gap-4">
+            @foreach($supplements as $supplement)
+                <div class="border border-black p-2 w-[300px]">
+                    <h3 class="text-lg font-semibold">{{ $supplement->name }}</h3>
+                    <p class="mt-0.5 text-gray-600 text-sm">{{ $supplement->description }}</p>
+                </div>
+            @endforeach
+        </div>
+    </section>
 </x-layout>
