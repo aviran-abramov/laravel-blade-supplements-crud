f68d4df1bc8fe1b57e5603fc51597b0a3bd19c10
Aviran Abramov <aviranabramov@gmail.com>
2024-11-25 18:37:35 +0200

    REFACTOR: Use resource method for routes

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,14 @@
 use App\Http\Controllers\SupplementController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(SupplementController::class)
-    ->prefix('supplements')
-    ->group(function () {
-        Route::get('/', 'index')->name('supplements.index');
-
-        Route::get('/create', 'create')->name('supplements.create');
-        Route::post('/', 'store')->name('supplements.store');
-
-        Route::get('/{supplement}', 'show')->name('supplements.show');
-
-        Route::get('/{supplement}/edit', 'edit')->name('supplements.edit');
-        Route::patch('/{supplement}', 'update')->name('supplements.update');
-
-        Route::delete("/{supplement}", 'destroy')->name('supplements.destroy');
-    });
+Route::resource('supplements', SupplementController::class)->names([
+    'index' => 'supplements.index',
+    'create' => 'supplements.create',
+    'store' => 'supplements.store',
+    'show' => 'supplements.show',
+    'edit' => 'supplements.edit',
+    'update' => 'supplements.update',
+    'delete' => 'supplements.delete',
+]);
 
 
