5b555f41b99929bd3f008842b21ced8a744b3e70
Aviran Abramov <aviranabramov@gmail.com>
2024-11-25 16:32:27 +0200

    Use prefix and group for supplements

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,16 @@
 use App\Http\Controllers\SupplementController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/supplements', [SupplementController::class, 'index'])->name('supplements.index');
+Route::prefix('supplements')->group(function () {
+    Route::get('/', [SupplementController::class, 'index'])->name('supplements.index');
 
-Route::get('/supplements/create', [SupplementController::class, 'create'])->name('supplements.create');
-Route::post('/supplements', [SupplementController::class, 'store'])->name('supplements.store');
+    Route::get('/create', [SupplementController::class, 'create'])->name('supplements.create');
+    Route::post('/', [SupplementController::class, 'store'])->name('supplements.store');
 
-Route::get('/supplements/{id}/edit', [SupplementController::class, 'edit'])->name('supplements.edit');
-Route::patch('/supplements/{id}', [SupplementController::class, 'update'])->name('supplement->update');
+    Route::get('/{id}/edit', [SupplementController::class, 'edit'])->name('supplements.edit');
+    Route::patch('/{id}', [SupplementController::class, 'update'])->name('supplement->update');
 
-Route::delete('/supplements/{id}', [SupplementController::class, 'destroy'])->name('supplement.destroy');
+    Route::delete('/{id}', [SupplementController::class, 'destroy'])->name('supplement.destroy');
+});
+
+
