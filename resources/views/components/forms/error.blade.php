e9d5bf0314c98112deff7e561c48baf01c9bd953
Aviran Abramov <aviranabramov@gmail.com>
2024-11-27 19:31:23 +0200

    Add attributes->merge to forms/error

--- resources/views/components/forms/error.blade.php
+++ resources/views/components/forms/error.blade.php
@@ -1,5 +1,5 @@
 @props(['name'])
 
 @error($name)
-    <p class="text-red-500 text-sm">* {{ $message }}</p>
+    <p {{ $attributes->merge(["class" => "text-red-500 text-sm"]) }}>* {{ $message }}</p>
 @enderror
